df9bcece1bdc40a0d669fb4d7ec2e194cdf1bd87
liuxiaorui <lxiaorui@users.noreply.github.com>
2022-01-26 13:33:25 -0500

    Create datasets.py

---
+++ datasets.py
@@ -0,0 +1,154 @@
+import os.path as osp
+import torch
+
+from torch_geometric.datasets import Planetoid, Coauthor, Amazon
+import torch_geometric.transforms as T
+from deeprobust.graph.data import Dataset, Dpr2Pyg
+
+import argparse
+
+def str2bool(v):
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Unsupported value encountered.')
+
+def uniqueId(dataset_name, target_node, perturbation):
+    return dataset_name.lower() +"_"+str(target_node)+"_"+str(perturbation)
+
+
+def prepare_data(args, lcc=False):
+    transform = T.ToSparseTensor()
+    if args.dataset == "Cora" or args.dataset == "CiteSeer" or args.dataset == "PubMed":
+        if lcc:
+            dpr_data = Dataset(root='/tmp/', name=(args.dataset).lower())
+            dataset = Dpr2Pyg(dpr_data, transform=transform)
+        else:
+            dataset = get_planetoid_dataset(args.dataset, args.normalize_features, transform)
+        permute_masks = random_planetoid_splits if args.random_splits else None
+
+    elif args.dataset == "cs" or args.dataset == "physics":
+        dataset = get_coauthor_dataset(args.dataset, args.normalize_features, transform)
+        permute_masks = random_coauthor_amazon_splits
+
+    elif args.dataset == "computers" or args.dataset == "photo":
+        dataset = get_amazon_dataset(args.dataset, args.normalize_features, transform)
+        permute_masks = random_coauthor_amazon_splits
+    print("Data:", dataset[0])
+
+    return dataset, permute_masks
+
+
+def get_planetoid_dataset(name, normalize_features=False, transform=None):
+    path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', name)
+    dataset = Planetoid(path, name)
+
+    if transform is not None and normalize_features:
+        dataset.transform = T.Compose([T.NormalizeFeatures(), transform])
+    elif normalize_features:
+        dataset.transform = T.NormalizeFeatures()
+    elif transform is not None:
+        dataset.transform = transform
+
+    return dataset
+
+
+def get_coauthor_dataset(name, normalize_features=False, transform=None):
+    path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', name)
+    dataset = Coauthor(path, name)
+
+    if transform is not None and normalize_features:
+        dataset.transform = T.Compose([T.NormalizeFeatures(), transform])
+    elif normalize_features:
+        dataset.transform = T.NormalizeFeatures()
+    elif transform is not None:
+        dataset.transform = transform
+
+    return dataset
+
+
+def get_amazon_dataset(name, normalize_features=False, transform=None):
+    path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', name)
+    dataset = Amazon(path, name)
+
+    if transform is not None and normalize_features:
+        dataset.transform = T.Compose([T.NormalizeFeatures(), transform])
+    elif normalize_features:
+        dataset.transform = T.NormalizeFeatures()
+    elif transform is not None:
+        dataset.transform = transform
+
+    return dataset
+
+
+def index_to_mask(index, size):
+    mask = torch.zeros(size, dtype=torch.bool, device=index.device)
+    mask[index] = 1
+    return mask
+
+
+def random_planetoid_splits(data, num_classes, lcc_mask=None):
+    # Set new random planetoid splits:
+    # * 20 * num_classes labels for training
+    # * 500 labels for validation
+    # * 1000 labels for testing
+
+    indices = []
+    if lcc_mask is not None:
+        for i in range(num_classes):
+            index = (data.y[lcc_mask] == i).nonzero().view(-1)
+            index = index[torch.randperm(index.size(0))]
+            indices.append(index)
+    else:
+        for i in range(num_classes):
+            index = (data.y == i).nonzero().view(-1)
+            index = index[torch.randperm(index.size(0))]
+            indices.append(index)
+
+    train_index = torch.cat([i[:20] for i in indices], dim=0)
+
+    rest_index = torch.cat([i[20:] for i in indices], dim=0)
+    rest_index = rest_index[torch.randperm(rest_index.size(0))]
+
+    data.train_mask = index_to_mask(train_index, size=data.num_nodes)
+    data.val_mask = index_to_mask(rest_index[:500], size=data.num_nodes)
+    data.test_mask = index_to_mask(rest_index[500:1500], size=data.num_nodes)
+
+    return data
+
+
+def random_coauthor_amazon_splits(data, num_classes, lcc_mask=None, seed=None):
+    # Set random coauthor/co-purchase splits:
+    # * 20 * num_classes labels for training
+    # * 30 * num_classes labels for validation
+    # rest labels for testing
+    g = None
+    if seed is not None:
+        g = torch.Generator()
+        g.manual_seed(seed)
+
+    indices = []
+    if lcc_mask is not None:
+        for i in range(num_classes):
+            index = (data.y[lcc_mask] == i).nonzero().view(-1)
+            index = index[torch.randperm(index.size(0), generator=g)]
+            indices.append(index)
+    else:
+        for i in range(num_classes):
+            index = (data.y == i).nonzero().view(-1)
+            index = index[torch.randperm(index.size(0), generator=g)]
+            indices.append(index)
+
+    train_index = torch.cat([i[:20] for i in indices], dim=0)
+    val_index = torch.cat([i[20:50] for i in indices], dim=0)
+
+    rest_index = torch.cat([i[50:] for i in indices], dim=0)
+    rest_index = rest_index[torch.randperm(rest_index.size(0))]
+
+    data.train_mask = index_to_mask(train_index, size=data.num_nodes)
+    data.val_mask = index_to_mask(val_index, size=data.num_nodes)
+    data.test_mask = index_to_mask(rest_index, size=data.num_nodes)
+
+    return data
